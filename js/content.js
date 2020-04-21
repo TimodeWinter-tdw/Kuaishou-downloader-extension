0293b9a4bd5f8de5f3449c19df22f03566ac3afd
Timo de Winter <info@timodw.nl>
2020-04-21 23:35:02 +0200

    Changed video URL gathering method.

--- js/content.js
+++ js/content.js
@@ -203,7 +203,7 @@ function prepareDownload(index) {
         if(!(document.getElementsByClassName("viewer-container-img").length > 0)) {
             console.log("Item seems to be a video.");
 
-            let videoURL = $("video")[0].context.currentSrc;
+            // let videoURL = $("video")[0].context.currentSrc;
             console.log("Executed function to get currentSrc. Src: " + videoURL);
 
             if (videoURL !== "" && videoURL !== undefined && videoURL !== null) {
