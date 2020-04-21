2401cb9386750e9bc6e4de9f5c263faa3246867d
Timo de Winter <info@timodw.nl>
2020-04-21 11:12:32 +0200

    Preparing for version 1.1

--- js/content.js
+++ js/content.js
@@ -1,7 +1,52 @@
-setInterval(function(){
-    $("html, body").animate({
-        scrollTop: $(
-            'html, body').get(0).scrollHeight
-    }, 2000);
- },
-2000);
+fireEvent = function (element, event) {
+    if (document.createEventObject) {
+        let evt = document.createEventObject();
+        return element.fireEvent("on" + event, evt);
+    } else {
+        let evt = document.createEvent("HTMLEvents");
+        evt.initEvent(event, true, true);
+        return !element.dispatchEvent(evt);
+    }
+};
+
+//Fires the click event
+jQuery.fn.fclick = function () {
+    $(this).each( function() {
+        fireEvent(this, "click");
+    });
+};
+
+// Functions to calculate time difference
+// Date 1 should be closest and date2 should be earliest
+function hourDiff(date1, date2) {
+    return Math.floor(Math.abs(date1.getTime() - date2.getTime()) / 3600000);
+}
+
+function dayDiff(date1, date2) {
+    return Math.abs((date1.getTime() - date2.getTime()) / (1000 * 3600 * 24));
+}
+
+function weekDiff(date1, date2) {
+    return Math.floor(dayDiff(date1, date2) / 7);
+}
+
+function monthDiff(date1, date2) {
+    var months;
+    months = (date1.getFullYear() - date2.getFullYear()) * 12;
+    months -= date2.getMonth()+1;
+    months += date1.getMonth();
+    return months <= 0 ? 0 : months;
+}
+
+function yearDiff(date1, date2) {
+    return date1.getFullYear() - date2.getFullYear();
+}
+
+// First scroll to bottom
+$(window).on("scroll", function() {
+    let scrollHeight = $(document).height();
+    let scrollPosition = $(window).height() + $(window).scrollTop();
+    if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+        console.log("At the bottom!");
+    }
+});
--- js/popup.js
+++ js/popup.js
@@ -2,18 +2,11 @@ $(document).ready(function(){
     $('#downloadBtn').on('click', function(){
         var videoUrl = $('#profileUrl').val();
         chrome.tabs.update({ url: videoUrl },function(){
-            
+
             setTimeout(update, 4000);
         });
 
         document.getElementById('download_div').style.display = "block";
-    });
-
-    $('#downloadVideos').on('click', function() {
-        // Launch the downloader
-        chrome.tabs.executeScript(null, {code: 'var startDate="' + $('#dateStart').val() + '";var endDate="' + $('#dateEnd').val() + '";'}, function() {
-            chrome.tabs.executeScript(null, {file: 'js/downloader2.js'});
-        });
     });
 
     chrome.runtime.onMessage.addListener(
@@ -26,13 +19,10 @@ $(document).ready(function(){
         sendResponse({message: "Video download started!"});
     });
 
-});
-
-function update()
-{
-    
-    chrome.tabs.executeScript(null, {file:'js/jquery.min.js'});
-    chrome.tabs.executeScript(null, {code: 'var databaseurl="'+ $('#text').val() + '";'}, function() {
+    function update() {
+        chrome.tabs.executeScript(null, {file:'js/jquery.min.js'});
+        chrome.tabs.executeScript(null, {code: 'let startDate="' + $('#dateStart').val() + '";let endDate="' + $('#dateEnd').val() + '";'}, function() {
             chrome.tabs.executeScript(null, {file: 'js/content.js'});
-       }); 
-}
+        });
+    }
+});
